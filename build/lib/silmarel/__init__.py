3aee98b67f6cac390093016d154d4b116d153d11
Laura Uronen <laura.uronen@gmail.com>
2026-01-28 12:07:58 +0100

    build up to date

--- build/lib/silmarel/__init__.py
+++ build/lib/silmarel/__init__.py
@@ -1,6 +1,8 @@
-from . import simulation
+import importlib.metadata
+
+from . import utils
 from . import data
 from . import likelihood
+from . import simulation
 from . import prior
-from . import postprocessing 
-from . import utils
+from . import postprocessing
